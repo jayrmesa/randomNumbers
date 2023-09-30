5f39b71f5f8fbe39d91ef905b9bdbbdd20b35c60
Arnold Mesa Jr <jayrmesa@gmail.com>
2023-09-30 16:15:32 -0600

    created component folder with the input values for the range

---
+++ src/components/InputValue.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function InputValues() {
+  return (
+    <div>
+      <input typ="text" placeholder="Enter range..." />
+      <button>Shuffle</button>
+    </div>
+  );
+}
+
+export default InputValues; 
+
+
