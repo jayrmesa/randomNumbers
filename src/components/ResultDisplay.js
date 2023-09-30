53dba3c82d159aa73f47974e179f333f6004c8b4
Arnold Mesa Jr <jayrmesa@gmail.com>
2023-09-30 16:18:15 -0600

    created result display for the intended results

--- src/components/ResultDisplay.js
+++ src/components/ResultDisplay.js
@@ -1,1 +1,14 @@
+import React from 'react';
 
+function ResultDisplay({ }) {
+  return (
+    <div>
+      <h2>Shuffled Numbers:</h2>
+      <ul>
+
+      </ul>
+    </div>
+  );
+}
+
+export default ResultDisplay;
