9f1bcb525e2ae2663fbb4b38342b8dd1e9603bae
Arnold Mesa Jr <jayrmesa@gmail.com>
2023-09-30 18:12:09 -0600

    chantge the message on the results

--- src/components/ResultDisplay.js
+++ src/components/ResultDisplay.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ResultDisplay({ shuffledNumbers }) {
   return (
     <div>
-      <h2>Shuffler:</h2>
+      <h2>Results:</h2>
       <ul>
         {shuffledNumbers.map((number, index) => (
           <li key={index}>{number}</li>
