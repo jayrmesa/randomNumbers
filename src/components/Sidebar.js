46b9102373bbd970cb5f0ac056318cd77b83b50e
Arnold Mesa Jr <jayrmesa@gmail.com>
2023-10-02 15:39:07 -0600

    refactor the code and seperate the sidebar about

---
+++ src/components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h2>About</h2>
+      <p>An app that generates a list of 10,000 numbers in random order each time it is run.</p>
+      <h3>Default range</h3>
+      <p>Min = 1</p>
+      <p>Max = 10,000</p>
+    </div>
+  );
+}
+
+export default Sidebar;
