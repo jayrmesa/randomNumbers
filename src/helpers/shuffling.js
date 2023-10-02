38e1ce178f05eb60e4e1d68c3246dc7cfa089e7e
Arnold Mesa Jr <jayrmesa@gmail.com>
2023-10-01 20:55:02 -0600

    Refractored some of the code to be structured instead having it all in the main file

--- src/helpers/shuffling.js
+++ src/helpers/shuffling.js
@@ -1,18 +1,17 @@
-// Create an array with numbers from min to max (inclusive)
-export function prepareArray(min, max) {
+// Create an array with numbers from min to max 
+export const arrayNum = (min, max) => {
   const uniqueNumbers = [];
   for (let i = min; i <= max; i++) {
     uniqueNumbers.push(i);
   }
   return uniqueNumbers;
-}
+};
 
-// Shuffle an array using the Fisher-Yates shuffle algorithm
-export function shuffleArray(array) {
+export const shuffleArr = (array) => {
+  // Fisher-Yates shuffle algorithm
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
-}
-
+};
