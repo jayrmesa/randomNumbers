2f6b289af415b7ba978aa068644dd18329140cf1
Arnold Mesa Jr <jayrmesa@gmail.com>
2023-10-02 16:38:49 -0600

    refactor the main file to seperate the functions

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import DownloadButton from './components/DownloadButton';
 
-import { validateInput } from './helpers/validation';
-import { arrayNum, shuffleArr } from './helpers/shuffling';
+import { handleInput } from './helpers/handleInput';
+import { handleShuffle } from './helpers/handleShuffle';
+import { saveResults } from './helpers/saveResults';
+
 import './App.css';
 
 function App() {
@@ -15,60 +17,6 @@ function App() {
   const [errorMsg, setErrorMsg] = useState('');
   const [shuffledNumbers, setShuffledNumbers] = useState([]);
   const [showResults, setShowResults] = useState(false);
-
-  const handleInput = (event) => {
-    const { name, value } = event.target;
-    handleValue(name, value);
-  };
-
-  const handleValue = (name, value) => {
-    const isValid = /^\d+$/.test(value);
-
-    if (isValid) {
-      if (name === 'min') {
-        setMin(parseInt(value, 10));
-      } else if (name === 'max') {
-        setMax(parseInt(value, 10));
-      }
-      setErrorMsg('');
-    } else if (value === '') {
-      clearInput(name);
-      setErrorMsg('Please enter a number');
-    } else {
-      setErrorMsg(validateInput(min, max));
-    }
-  };
-
-  const clearInput = (name) => {
-    if (name === 'min') {
-      setMin('');
-    } else if (name === 'max') {
-      setMax('');
-    }
-  };
-
-  const handleShuffle = () => {
-    const errorMessage = validateInput(min, max);
-
-    if (errorMessage) {
-      setErrorMsg(errorMessage);
-      setShowResults(false);
-    } else {
-      const uniqueNumbers = arrayNum(min, max);
-      const shuffledNumbers = shuffleArr(uniqueNumbers);
-      setShuffledNumbers(shuffledNumbers);
-      setShowResults(true);
-    }
-  };
-
-  const saveResults = () => {
-    const resultsText = shuffledNumbers.join('\n');
-    const blob = new Blob([resultsText], { type: 'text/plain' });
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(blob);
-    link.download = 'shuffled_numbers.txt';
-    link.click();
-  };
 
   return (
     <div>
@@ -79,9 +27,9 @@ function App() {
         <InputValue
           min={min}
           max={max}
-          onInput={handleInput}
+          onInput={(event) => handleInput(event, { min, max, setMin, setMax, setErrorMsg })}
         />
-        <button onClick={handleShuffle}>Shuffle</button>
+        <button onClick={() => handleShuffle({ min, max, setErrorMsg, setShuffledNumbers, setShowResults })}>Shuffle</button>
         {errorMsg && <div className="error">{errorMsg}</div>}
         {showResults && !errorMsg && <ResultDisplay shuffledNumbers={shuffledNumbers} />}
         {showResults && !errorMsg && <DownloadButton onClick={() => saveResults(shuffledNumbers)} />}
