84e9d9d3693c782c67a617c4d3f3abfffe2d805d
Arnold Mesa Jr <jayrmesa@gmail.com>
2023-10-02 16:37:47 -0600

    refactor the code, into readability and organization

---
+++ src/helpers/saveResults.js
@@ -0,0 +1,10 @@
+export function saveResults(shuffledNumbers) {
+  const resultsText = shuffledNumbers.join('\n');
+  const blob = new Blob([resultsText], { type: 'text/plain' });
+  const link = document.createElement('a');
+  link.href = URL.createObjectURL(blob);
+  link.download = 'shuffled_numbers.txt';
+  link.click();
+}
+
+
