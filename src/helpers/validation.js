ac7636c25209a14be7610fbc009a53ea90db3e4d
Arnold Mesa Jr <jayrmesa@gmail.com>
2023-10-01 20:56:10 -0600

    Refractored some of the code to be structured also fix the error to properly show up and add more validation input for erros

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -1,28 +1,16 @@
-// Validate if the input values are within the specified range
-export function validateInput(min, max) {
-  if (min >= 1 && max <= 10000 && min <= max) {
-    return true;
-  } else {
-    return false;
-  }
-}
+// Check the input values are within the range and return an error if not
 
-// Handle invalid input with appropriate error messages
-export function handleInvalidInput(minValue, maxValue) {
-  if (minValue < 1 || maxValue > 10000) {
-    alert('Please enter values within the range [1, 10,000].');
-  } else if (minValue > maxValue) {
-    alert('The minimum value must be less than or equal to the maximum value.');
+export const validateInput = (min, max) => {
+  if (min === '' || max === '') {
+    return 'Please enter both minimum and maximum values.';
+  } else if (min === max) {
+    return 'Minimum and maximum values cannot be the same.';
+  } else if (min < 1 || max > 10000) {
+    return 'Please enter values within the range of 1 to 10,000.';
+  } else if (min > max) {
+    return 'The minimum value must be less than to the maximum value.';
   } else {
-    alert('Invalid input. Please enter valid values.');
+    return ''; // No error
   }
-}
+};
 
-// Clear input value if it's empty
-export function clearInputValue(name, setMinValue, setMaxValue) {
-  if (name === 'minValue') {
-    setMinValue('');
-  } else if (name === 'maxValue') {
-    setMaxValue('');
-  }
-}
