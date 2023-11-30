e29a217b04c06d4b0e1dd4762fee575fe90c21bd
Arnold Mesa Jr <jayrmesa@gmail.com>
2023-11-30 10:52:52 -0700

    Fix the shuffling algorim side effect and created a copy of the array

--- src/helpers/shuffling.js
+++ src/helpers/shuffling.js
@@ -8,10 +8,12 @@ export const arrayNum = (min, max) => {
 };
 
 export const shuffleArr = (array) => {
+  let arrayCopy = [...array]; 
   // Fisher-Yates shuffle algorithm
-  for (let i = array.length - 1; i > 0; i--) {
+  for (let i = arrayCopy.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+    [arrayCopy[i], arrayCopy[j]] = [arrayCopy[j], arrayCopy[i]];
   }
-  return array;
+  return arrayCopy;
 };
+
